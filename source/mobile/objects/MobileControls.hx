c199e47ff2fc0c40dfc9da4e8b38ee34f4f68863
Ethantobot <ethantobot7@gmail.com>
2026-05-22 18:27:44 +0200

    Update MobileControls.hx

--- source/mobile/objects/MobileControls.hx
+++ source/mobile/objects/MobileControls.hx
@@ -206,7 +206,7 @@ class CurrentManager {
 	public var buttonExtra4:MobileButton;
 
 	public function new(control:MobileControls){
-		if(MobileControls.mode == HITBOX && ClientPrefs.hitboxmode != 'Classic') {
+		if(MobileControls.mode == HITBOX && ClientPrefs.data.hitboxmode != 'Classic') {
 			buttonLeft = control.newhbox.buttonLeft;
 			buttonDown = control.newhbox.buttonDown;
 			buttonUp = control.newhbox.buttonUp;
@@ -215,14 +215,14 @@ class CurrentManager {
 			buttonExtra2 = control.newhbox.buttonExtra2;
 			buttonExtra3 = control.newhbox.buttonExtra3;
 			buttonExtra4 = control.newhbox.buttonExtra4;
-		} else if(MobileControls.mode == HITBOX && ClientPrefs.hitboxmode == 'Classic') { //Classic Hitbox Now Support Shift & Space Buttons
+		} else if(MobileControls.mode == HITBOX && ClientPrefs.data.hitboxmode == 'Classic') { //Classic Hitbox Now Support Shift & Space Buttons
 			buttonLeft = control.hbox.buttonLeft;
 			buttonDown = control.hbox.buttonDown;
 			buttonUp = control.hbox.buttonUp;
 			buttonRight = control.hbox.buttonRight;
 			buttonExtra1 = control.hbox.buttonExtra1;
 			buttonExtra2 = control.hbox.buttonExtra2;
-		} else if (ClientPrefs.hitboxmode != 'Classic' && MobileControls.mode != KEYBOARD) {
+		} else if (ClientPrefs.data.hitboxmode != 'Classic' && MobileControls.mode != KEYBOARD) {
 			buttonLeft = control.vpad.buttonLeft;
 			buttonDown = control.vpad.buttonDown;
 			buttonUp = control.vpad.buttonUp;
