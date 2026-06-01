8eb89149c6b57d574f211f834ba570f21a0e5511
Ethantobot <ethantobot7@gmail.com>
2026-06-01 21:20:15 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -118,7 +118,7 @@ class Main extends Sprite
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
 		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
-		addChild(new FlxGame(game.width, game.height,#if mobile CopyState.checkExistingFiles() ?  CopyState : #end game.initialState #end, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+		addChild(new FlxGame(game.width, game.height,#if mobile CopyState.checkExistingFiles() ?  CopyState : #end game.initialState , #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
 		#if !mobile
 		fpsVar = new FPSCounter(10, 3, 0xFF4BD8);
