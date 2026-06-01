b0c4214ca68c8ca946e35087bf8281df34ab4f06
Ethantobot <ethantobot7@gmail.com>
2026-06-01 17:50:26 +0200

    Update CreditsState.hx

--- source/states/CreditsState.hx
+++ source/states/CreditsState.hx
@@ -114,6 +114,9 @@ class CreditsState extends MusicBeatState
 		bg.color = CoolUtil.colorFromString(creditsStuff[curSelected][4]);
 		intendedColor = bg.color;
 		changeSelection();
+		#if TOUCH_CONTROLS
+        addMobilePad("UP_DOWN", "A_B");
+        #end
 		super.create();
 	}
 
