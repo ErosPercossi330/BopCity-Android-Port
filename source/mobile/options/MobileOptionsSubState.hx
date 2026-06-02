ac25044f1d79077fac8b049dc5f740129cbc118c
Ethantobot <ethantobot7@gmail.com>
2026-06-02 11:41:42 +0200

    Update MobileOptionsSubState.hx

--- source/mobile/options/MobileOptionsSubState.hx
+++ source/mobile/options/MobileOptionsSubState.hx
@@ -7,7 +7,6 @@ import openfl.text.TextField;
 import flixel.addons.display.FlxGridOverlay;
 import lime.utils.Assets;
 import flixel.FlxSubState;
-import openfl.text.TextField;
 import flixel.util.FlxSave;
 import haxe.Json;
 import flixel.input.keyboard.FlxKey;
@@ -20,7 +19,6 @@ import haxe.io.Path;
 import sys.FileSystem;
 import sys.io.File;
 import mobile.backend.StorageUtil;
-
 
 class MobileOptionsSubState extends BaseOptionsMenu
 {
@@ -38,106 +36,110 @@ class MobileOptionsSubState extends BaseOptionsMenu
 		storageTypes = storageTypes.concat(externalPaths); //SD Card
 		#end
 		title = 'Mobile Options';
-		rpcTitle = 'Mobile Options Menu'; //hi, you can ask what is that, i will answer it's all what you needed lol.
+		rpcTitle = 'Mobile Options Menu'; 
+		
 		#if TOUCH_CONTROLS
 		HitboxTypes = mergeAllTextsNamed('mobile/Hitbox/HitboxModes/hitboxModeList.txt');
 		#end
 
-	#if TOUCH_CONTROLS
-	var option:Option = new Option('MobilePad Alpha:',
-		'Changes MobilePad Alpha -cool feature',
-		'mobilePadAlpha',
-		'percent',
-		0.6);
-	option.scrollSpeed = 1.6;
-	option.minValue = 0;
-	option.maxValue = 1;
-	option.changeValue = 0.1;
-	option.decimals = 1;
-	option.onChange = () ->
-	{
-		mobilePad.alpha = curOption.getValue();
-	};
-	addOption(option);
-	super();
+		var option:Option = null;
 
-	var option:Option = new Option('Extra Controls',
-		"Allow Extra Controls",
-		'extraKeys',
-		'float',
-		2);
-	option.scrollSpeed = 1.6;
-	option.minValue = 0;
-	option.maxValue = 4;
-	option.changeValue = 1;
-	option.decimals = 1;
-	addOption(option);
-
-	var option:Option = new Option('Extra Control Location:',
-		"Choose Extra Control Location",
-		'hitboxLocation',
-		'string',
-		'Bottom',
-		['Bottom', 'Top', 'Middle']);
-	addOption(option);
-
-	HitboxTypes.insert(0, "New");
-	HitboxTypes.insert(0, "Classic");
-	var option:Option = new Option('Hitbox Mode:',
-		"Choose your Hitbox Style! -mariomaster",
-		'hitboxmode',
-		'string',
-		'New',
-		HitboxTypes);
-	addOption(option);
-
-	var option:Option = new Option('Hitbox Design:',
-		"Choose how your hitbox should look like.",
-		'hitboxtype',
-		'string',
-		'Gradient',
-		['Gradient', 'No Gradient' , 'No Gradient (Old)']);
-	addOption(option);
-
-	var option:Option = new Option('Hitbox Hint',
-		'Hitbox Hint -I hate this',
-		'hitboxhint',
-		'bool',
-		false);
-	addOption(option);
-
-	var option:Option = new Option('Hitbox Opacity', //mariomaster was here again -I won't remove this because... Y'know This is here on almost 1 year
-		'Changes hitbox opacity -omg',
-		'hitboxalpha',
-		'float',
-		0.7);
-	option.scrollSpeed = 1.6;
-	option.minValue = 0.0;
-	option.maxValue = 1;
-	option.changeValue = 0.1;
-	option.decimals = 1;
-	addOption(option);
-	#end
-
-	#if mobile
-	var option:Option = new Option('Wide Screen Mode',
-		'If checked, The game will stetch to fill your whole screen. (WARNING: Can result in bad visuals & break some mods that resizes the game/cameras)',
-		'wideScreen',
-		'bool',
-		false);
-	option.onChange = () -> FlxG.scaleMode = new MobileScaleMode();
-	addOption(option);
-	#end
-
-	#if android
-	var option:Option = new Option('Storage Type',
-		'Which folder Psych Engine should use?',
-		'storageType',
-		'string',
-		null,
-		storageTypes);
+		#if TOUCH_CONTROLS
+		option = new Option('MobilePad Alpha:',
+			'Changes MobilePad Alpha -cool feature',
+			'mobilePadAlpha',
+			'percent',
+			0.6);
+		option.scrollSpeed = 1.6;
+		option.minValue = 0;
+		option.maxValue = 1;
+		option.changeValue = 0.1;
+		option.decimals = 1;
+		option.onChange = () ->
+		{
+			if(mobilePad != null) mobilePad.alpha = curOption.getValue();
+		};
 		addOption(option);
-	#end
+
+		option = new Option('Extra Controls',
+			"Allow Extra Controls",
+			'extraKeys',
+			'float',
+			2);
+		option.scrollSpeed = 1.6;
+		option.minValue = 0;
+		option.maxValue = 4;
+		option.changeValue = 1;
+		option.decimals = 1;
+		addOption(option);
+
+		option = new Option('Extra Control Location:',
+			"Choose Extra Control Location",
+			'hitboxLocation',
+			'string',
+			'Bottom',
+			['Bottom', 'Top', 'Middle']);
+		addOption(option);
+
+		if(HitboxTypes == null) HitboxTypes = [];
+		if(!HitboxTypes.contains("New")) HitboxTypes.insert(0, "New");
+		if(!HitboxTypes.contains("Classic")) HitboxTypes.insert(0, "Classic");
+		
+		option = new Option('Hitbox Mode:',
+			"Choose your Hitbox Style! -mariomaster",
+			'hitboxmode',
+			'string',
+			'New',
+			HitboxTypes);
+		addOption(option);
+
+		option = new Option('Hitbox Design:',
+			"Choose how your hitbox should look like.",
+			'hitboxtype',
+			'string',
+			'Gradient',
+			['Gradient', 'No Gradient' , 'No Gradient (Old)']);
+		addOption(option);
+
+		option = new Option('Hitbox Hint',
+			'Hitbox Hint -I hate this',
+			'hitboxhint',
+			'bool',
+			false);
+		addOption(option);
+
+		option = new Option('Hitbox Opacity', 
+			'Changes hitbox opacity -omg',
+			'hitboxalpha',
+			'float',
+			0.7);
+		option.scrollSpeed = 1.6;
+		option.minValue = 0.0;
+		option.maxValue = 1;
+		option.changeValue = 0.1;
+		option.decimals = 1;
+		addOption(option);
+		#end
+
+		#if mobile
+		option = new Option('Wide Screen Mode',
+			'If checked, The game will stretch to fill your whole screen. (WARNING: Can result in bad visuals & break some mods that resizes the game/cameras)',
+			'wideScreen',
+			'bool',
+			false);
+		option.onChange = () -> FlxG.scaleMode = new MobileScaleMode();
+		addOption(option);
+		#end
+
+		#if android
+		option = new Option('Storage Type',
+			'Which folder Psych Engine should use?',
+			'storageType',
+			'string',
+			null,
+			storageTypes);
+		addOption(option);
+		#end
 
 		super();
 	}
@@ -212,14 +214,14 @@ class MobileOptionsSubState extends BaseOptionsMenu
 				if(FileSystem.exists(folder) && !foldersToCheck.contains(folder)) foldersToCheck.push(folder);
 			}
 
-			// Then "PsychEngine/mods/" main folder
+			// Then "mods/" main folder
 			var folder:String = Paths.mods(fileToFind);
 			if(FileSystem.exists(folder) && !foldersToCheck.contains(folder)) foldersToCheck.push(Paths.mods(fileToFind));
 
-			// And lastly, the loaded mod's folder
-			if(Paths.currentModDirectory != null && Mods.currentModDirectory.length > 0)
+			// And lastly, the loaded mod's folder (Converted to unified 0.7+ Mods backend references)
+			if(Mods.currentModDirectory != null && Mods.currentModDirectory.length > 0)
 			{
-				var folder:String = Paths.mods(Paths.currentModDirectory + '/' + fileToFind);
+				var folder:String = Paths.mods(Mods.currentModDirectory + '/' + fileToFind);
 				if(FileSystem.exists(folder) && !foldersToCheck.contains(folder)) foldersToCheck.push(folder);
 			}
 		}
