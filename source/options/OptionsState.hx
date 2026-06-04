8f312e6b4f59001fd144d6994552fc4e61d9071c
Ethantobot <ethantobot7@gmail.com>
2026-06-04 14:51:48 +0200

    lets try this is my last fucking hope for the B soft lock

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -92,6 +92,7 @@ class OptionsState extends MusicBeatState
 		ClientPrefs.saveSettings();
 		#if TOUCH_CONTROLS
 		removeMobilePad();
+		addMobilePad("UP_DOWN", "A_B_C");
 		persistentUpdate = true;
 		#end
 		#if DISCORD_ALLOWED
