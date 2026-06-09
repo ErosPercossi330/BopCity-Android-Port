9aca94fdea3c488a8e9730d1494ebcd7f6cba2a1
Ethantobot <ethantobot7@gmail.com>
2026-06-09 14:21:52 +0200

    hope this fix context 3D ?

--- source/openfl/display3D/Context3D.hx
+++ source/openfl/display3D/Context3D.hx
@@ -460,6 +460,10 @@ import lime.math.Vector2;
 		dispose() or because the underlying rendering hardware has been lost.
 		@throws	Error	3768: The Stage3D API may not be used during background execution.
 	**/
+	@:noCompletion private inline function __clear(red:Float = 0, green:Float = 0, blue:Float = 0, alpha:Float = 1, depth:Float = 1, stencil:UInt = 0, mask:Int = 7):Void
+    {
+        clear(red, green, blue, alpha, depth, stencil, cast mask);
+    }
 	public function clear(red:Float = 0, green:Float = 0, blue:Float = 0, alpha:Float = 1, depth:Float = 1, stencil:UInt = 0,
 			mask:UInt = Context3DClearMask.ALL):Void
 	{
